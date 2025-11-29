f2f46e8ee4cc58b137869a9bf71caf79886637d0
Jainish Patel <64199640+jainishpatel2106@users.noreply.github.com>
2025-11-29 23:53:11 +0530

    Create ESP_stup.ino

---
+++ Arduino/ESP_stup.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(115200);   // PC
+  Serial1.begin(9600);    // ESP8266
+}
+
+void loop() {
+  if (Serial.available()) Serial1.write(Serial.read());
+  if (Serial1.available()) Serial.write(Serial1.read());
+}
